1215595fda5ca96556c8752ee81273792e0aee2e
nabilhayet <nabil4457@yahoo.com>
2023-11-15 15:30:50 -0500

    added validation in user and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
+
+  validates :date, presence: true
+  validates :time, presence: true
+  # validates :time, uniqueness: { scope: [:date, :cafe],
+  #   message: "You can't make a booking at this time!" }
+  validates :user_id, presence: true
+  validates :table_num, presence: true
+  validates :table_num, numericality: { other_than: 0 }
+  validates :table_num, numericality: { only_integer: true }
+
+  scope :with_date, -> { order("date asc") }
+  scope :with_table_num, -> { order("table_num desc") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_many :bookings
     accepts_nested_attributes_for :bookings
+
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :address, uniqueness: true
+    validates :contact, presence: true
+    validates :contact, uniqueness: true
+    validates :contact, numericality: { only_integer: true }
 end
